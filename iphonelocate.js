674ab82f0dd311fd4c0b07db7f22c86e9c4728dd
robert <robert@arles.us>
2017-06-27 17:39:54 -0700

    Initial Commit. working iphone location and write to file

---
+++ iphonelocate.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const os = require('os') 
+var icloud = require("find-my-iphone").findmyphone;
+
+const icloudConfFile = `${os.homedir()}/.rabot/icloud.conf`;
+const iphoneLocationFile = `${os.homedir()}/.rabot/location.json`;
+icloud.apple_id = "";
+icloud.password = "";
+
+exports.retrieveCredentials = ()=>{
+    try{
+        let icloudConf = fs.readFileSync(icloudConfFile).toString();
+        [icloud.apple_id, icloud.password] = icloudConf.trim().split(',');
+    }catch(e){
+        console.error('retrieveCredentials caught exception');
+        console.dir(e);
+    }
+}
+
+
+exports.getLocation = (deviceName)=>{
+    let location = {isSet: false, message: 'set me!'};
+    try{
+        icloud.getDevices(function(error, devices) {
+            var device;
+            let ourDevicesSample = ['iPhone ra','iPad ra','sinspare-7','ambp','mini','iPhone AA','ambp', 'Andrea\'s iPad', 'Katelyn\'s iPhone', 'Robert’s MacBook Pro'];
+            if(error){
+                throw(error);
+            }else{
+                devices.forEach((device)=>{
+                    try{
+                        if(device.name.includes(deviceName)){
+                            location = device.location;
+                        }
+                    }catch(e){
+                        console.error('getLocation caught exception while iterating devices')
+                        console.dir(e);
+                    }
+                });
+            }
+        });
+    }catch(e){
+        console.error('getLocation caught exception while trying to icloud.getDevices');
+        console.dir(e);
+    }
+    return(location);
+}
+
+exports.writeLocation = (location)=>{
+    try{
+        fs.writeFileSync(iphoneLocationFile, JSON.stringify(location));
+    }catch(e){
+        console.error('writeLocation caught exception');
+        console.dir(e);
+    }
+
+}
+
+exports.retrieveCredentials();
+let location = exports.getLocation("iPhone ra");
+exports.writeLocation(location);
